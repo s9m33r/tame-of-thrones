1f5d27f1d2ee6b6195a9379acbeb2cf63e82f8db
Sam Hokage <samhokage@gmail.com>
2018-12-01 16:23:18 +0530

    implemented send message

--- src/main/java/inc/hokage/Problem1.java
+++ src/main/java/inc/hokage/Problem1.java
@@ -3,19 +3,16 @@ package inc.hokage;
 import inc.hokage.model.King;
 import inc.hokage.model.Kingdom;
 
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Scanner;
 
-import static java.util.Objects.isNull;
-
 public class Problem1 {
     private static Universe universe;
+    private static Scanner scanner = new Scanner(System.in);
+    private static King kingShan;
 
     public static void main(String[] args) {
         setUpTheUniverseOfSoutheros();
-
-        Scanner scanner = new Scanner(System.in);
 
         String userChoice;
         do{
@@ -45,33 +42,33 @@ public class Problem1 {
         universe = new Universe();
 
         Kingdom spaceKingdom = new Kingdom("Space", "Gorilla");
-        King kingShan = new King("Shan", spaceKingdom);
+        kingShan = new King("Shan", spaceKingdom);
         spaceKingdom.makeKing(kingShan);
         universe.addKingdom(spaceKingdom);
 
         Kingdom landKingdom = new Kingdom("Land", "Panda");
         King kingMax = new King("Max", landKingdom);
-        spaceKingdom.makeKing(kingMax);
+        landKingdom.makeKing(kingMax);
         universe.addKingdom(landKingdom);
 
         Kingdom waterKingdom = new Kingdom("Water", "Octopus");
         King kingJade = new King("Jade", waterKingdom);
-        spaceKingdom.makeKing(kingJade);
+        waterKingdom.makeKing(kingJade);
         universe.addKingdom(waterKingdom);
 
         Kingdom iceKingdom = new Kingdom("Ice", "Mammoth");
         King kingMinn = new King("Minn", iceKingdom);
-        spaceKingdom.makeKing(kingMinn);
+        iceKingdom.makeKing(kingMinn);
         universe.addKingdom(iceKingdom);
 
         Kingdom airKingdom = new Kingdom("Air", "Owl");
         King kingShaw = new King("Shaw", airKingdom);
-        spaceKingdom.makeKing(kingShaw);
+        airKingdom.makeKing(kingShaw);
         universe.addKingdom(airKingdom);
 
         Kingdom fireKingdom = new Kingdom("Fire", "Dragon");
         King kingHank = new King("Hank", fireKingdom);
-        spaceKingdom.makeKing(kingHank);
+        fireKingdom.makeKing(kingHank);
         universe.addKingdom(fireKingdom);
     }
 
@@ -87,6 +84,18 @@ public class Problem1 {
     }
 
     private static void sendMessage() {
+        System.out.print("Enter message: ");
+        String message = scanner.nextLine();
+        System.out.print("To(Name of the Kingdom): ");
+        String kingdomName = scanner.nextLine();
+
+        Optional<Kingdom> kingdom = universe.getKingdomByName(kingdomName);
+
+        if(kingdom.isPresent()) {
+            System.out.println(String.format("Message sent to %s kingdom!", kingdomName));
+            kingShan.sendAllegianceInvitation(message, kingdom.get());
+        } else
+            System.out.println("There is no such kingdom");
     }
 
     private static void printExitMessage() {
